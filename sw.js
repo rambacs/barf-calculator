a352f2de91476c6e2aa4be3fa2b16ab3bbe7b981
rambacs <113919238+rambacs@users.noreply.github.com>
2025-12-25 19:52:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ const CACHE_NAME = "barf-calculator-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
@@ -11,10 +13,22 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match("./"))
   );
 });
